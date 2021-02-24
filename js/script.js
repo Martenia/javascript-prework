3022910a0d2d9a811353b6039328f4e6eabfc637
Marta Nitek <marta.nitek@onet.pl>
2021-02-24 20:09:30 +0100

    Zmienne

--- js/script.js
+++ js/script.js
@@ -1,50 +1,13 @@
-/**
-var computerMove, randomNumber;
-randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('Wylosowana liczba to: ' + randomNumber);
-if (randomNumber == '1') {
-  computerMove = 'kamień';
-} 
-else if (randomNumber == '2') {
-    computerMove = 'papier';
-}
-else if (randomNumber == '3') {
-    computerMove = 'nożyce';
-}
-printMessage('Mój ruch: ' + computerMove);
-
-var playerMove, playerInput;
-playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('Wpisana odpowiedź to: ' + playerInput);
-if (playerInput == '1') {
-    playerMove = 'kamień';
-} 
-else if (playerInput == '2') {
-    playerMove = 'papier';  
-}
-else if (playerInput == '3') {
-    playerMove = 'nożyce'
-}
-else {
-    printMessage('Nieznany ruch. Zakładam, że chodziło o "kamień".');
-    playerMove = 'kamień';
-}
-printMessage('Twój ruch: ' + playerMove);
-
-var argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput;
-*/
-
-
-var argButtonName, buttonTest, buttonRock, buttonPaper, buttonScissors;
-var x = 0;
-var y = 0;
+let argButtonName, buttonTest, buttonRock, buttonPaper, buttonScissors;
+let x = 0, 
+    y = 0;
 
 /** buttonTest = document.getElementById('button-test'); */
 buttonRock = document.getElementById('button-rock');
 buttonPaper = document.getElementById('button-paper');
 buttonScissors = document.getElementById('button-scissors');
 
-function buttonClicked(argButtonName) {
+function buttonClicked(argButtonName) { 
   clearMessages();
   console.log(argButtonName + ' został kliknięty');
 
@@ -103,4 +66,3 @@ function buttonClicked(argButtonName) {
 buttonRock.addEventListener('click', function(){ buttonClicked('kamień'); });
 buttonPaper.addEventListener('click', function(){ buttonClicked('papier'); });
 buttonScissors.addEventListener('click', function(){ buttonClicked('nożyce'); });
-
